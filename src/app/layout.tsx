6468c0c8e8f2e8a5873e9b3c2ff5ad68e71e730d
Project Team <project.teamedit@gmail.com>
2025-09-11 06:47:09 +0000

    Try fixing this error: `Runtime Error: Error: Failed to load chunk /_nex

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,13 +18,13 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" suppressHydrationWarning>
+    <html lang="en" suppressHydrationWarning className="h-full">
       <head>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=PT+Sans:wght@400;700&display=swap" rel="stylesheet" />
       </head>
-      <body className={cn('font-body antialiased h-screen')}>
+      <body className={cn('font-body antialiased h-full')}>
         <AuthProvider>
           {children}
           <Toaster />
